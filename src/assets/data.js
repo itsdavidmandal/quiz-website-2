93405a23fc7ed0d4fafad40a66bf9157b7d2a816
David Ramovich Mandal <davidramovichmandal@gmail.com>
2024-06-04 00:54:59 +0545

    initial commit

---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+        question: "Which planet is known as the Red Planet?",
+        option1: "Earth",
+        option2: "Mars",
+        option3: "Jupiter",
+        option4: "Venus",
+        ans: 2
+    },
+    {
+        question: "What is the capital of France?",
+        option1: "Berlin",
+        option2: "Madrid",
+        option3: "Paris",
+        option4: "Lisbon",
+        ans: 3
+    },
+    {
+        question: "Which element is needed for the process of photosynthesis?",
+        option1: "Nitrogen",
+        option2: "Oxygen",
+        option3: "Hydrogen",
+        option4: "Carbon Dioxide",
+        ans: 4
+    },
+    {
+        question: "What is the largest mammal in the world?",
+        option1: "Elephant",
+        option2: "Blue Whale",
+        option3: "Giraffe",
+        option4: "Shark",
+        ans: 2
+    },
+    {
+        question: "Who wrote the play 'Romeo and Juliet'?",
+        option1: "William Shakespeare",
+        option2: "Jane Austen",
+        option3: "Charles Dickens",
+        option4: "Mark Twain",
+        ans: 1
+    }
+];
